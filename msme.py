f5a311a10a0502cb0eb973d7df453e6630f9e166
gis4good <105337168+gis4good@users.noreply.github.com>
2022-06-29 16:26:04 +0530

    Update msme.py

--- msme.py
+++ msme.py
@@ -17,18 +17,6 @@ def app():
     HtmlFile = open("msme.html", 'r', encoding='utf-8')
     source_code = HtmlFile.read() 
     components.html(source_code,height =800,width=600)
-    
-    page_bg_img = '''
-    <style>
-    body {
-    background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
-    background-size: cover;
-    }
-    </style>
-    '''
-    
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-    
     def set_bg_hack_url():
         '''
         A function to unpack an image from url and set as bg.
