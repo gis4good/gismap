faa56fa242d183ce201c81d19c1938f9cda6345a
gis4good <105337168+gis4good@users.noreply.github.com>
2022-11-11 11:57:19 +0530

    Update map.py

--- map.py
+++ map.py
@@ -5,7 +5,7 @@ Created on Tue Jun 28 10:37:34 2022
 @author: THIS-LAPPY
 """
 
-import streamlit as st,msme,main,demo
+import streamlit as st,msme,main,demo,om
 import streamlit.components.v1 as components
 
 # >>> import plotly.express as px
