29a78c643404520cbd13574c81e0434019e8ef4c
gis4good <105337168+gis4good@users.noreply.github.com>
2022-11-11 11:37:37 +0530

    Add files via upload

---
+++ om.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 11 09:38:09 2022
+
+@author: THIS-LAPPY
+"""
+
+import streamlit as st,numpy as np
+import streamlit.components.v1 as components
+import requests
+
+def app():
+    
+    
+    st.header("""
+    Welcome to Annapurna Finance Pvt Ltd - Branch Split Dashboard
+    """)
+    # streamlit_app.py
+    
+    
+    def set_bg_hack_url():
+         '''
+         A function to unpack an image from url and set as bg.
+         Returns
+         -------
+         The background.
+         '''
+             
+         st.markdown(
+              f"""
+              <style>
+              .stApp {{
+                  background: url("https://i.pinimg.com/originals/b9/c8/f8/b9c8f893c9a782033a01f47e0c0b1d6e.jpg");
+                  background-size: cover
+              }}
+              </style>
+              """,
+              unsafe_allow_html=True
+          )
+    set_bg_hack_url() 
+    st.header('')
+    st.write("""
+    # OPerational Map
+    Image depecting villages covered by Branches. 
+    """)
+    import pandas as pd  
+    df = pd.read_csv(r"C:\Users\THIS-LAPPY\Desktop\gis\sharable_link_for_mail\od_maps_shareable_links.csv")
+    image_list = df.iloc[:, 1].unique().tolist()
+    item_list = df.iloc[:, 0].unique().tolist()
+    images = dict(zip(item_list, image_list))
+
+    user_option = st.selectbox('Choose your Branch',item_list)
+    print(user_option)
+    gg=df[df['Filename']==user_option].reset_index(drop=True)
+    # st.image(gg['link'][0], width = 400)
+    # st.components.v1.iframe(gg['link'][0], width = 1500, height = 800, scrolling = True)
+    # st.image(gg['link'][0].replace('dl=0','raw=1'))
+    # st.markdown(gg['link'][0], unsafe_allow_html=True)
+    st.markdown('For Download High Res = '+gg['link'][0].replace('raw=1','dl=0'), unsafe_allow_html=True)
+    st.image(gg['link'][0].replace('dl=0','raw=1'))
+
+
