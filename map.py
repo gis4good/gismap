017306544fe1aa68be867f6c357408d806fe4e20
gis4good <105337168+gis4good@users.noreply.github.com>
2023-02-09 10:56:17 +0530

    Update map.py

--- map.py
+++ map.py
@@ -5,7 +5,7 @@ Created on Tue Jun 28 10:37:34 2022
 @author: THIS-LAPPY
 """
 
-import streamlit as st,msme,main,demo,om,ism,bpm,od,bpm_sheets,equifax,isr
+import streamlit as st,msme,main,demo,om,ism,bpm,od,bpm_sheets,equifax,isr,summary_table
 import streamlit.components.v1 as components
 
 # >>> import plotly.express as px
@@ -82,7 +82,8 @@ if check_password():
     "ISM Maps":ism,
     "OD Maps":od,
     "Equifax Processor":equifax,
-    "ISR Processor":isr    
+    "ISR Processor":isr,
+    "Branch Split Processor":summary_table    
     }
     st.sidebar.title('Navigation')
     selection = st.sidebar.radio("Go to", list(PAGES.keys()))
